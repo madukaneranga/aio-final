753d0838e61d4880c1d00ea740384474b8f5a7bf
MadukaNernaga <141767341+MadukaNernaga@users.noreply.github.com>
2025-07-11 13:58:01 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -29,7 +29,26 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+//app.use(cors());
+const allowedOrigins = ['https://aio-final.vercel.app'];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    // allow requests with no origin (like curl, Postman)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true, // only enable if you use cookies or authorization headers
+}));
+
+
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
