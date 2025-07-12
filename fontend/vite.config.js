b552702a175bcedfcf458a86df15096338f77666
MadukaNernaga <141767341+MadukaNernaga@users.noreply.github.com>
2025-07-12 11:43:38 +0530

    error fixed for admin

--- fontend/vite.config.js
+++ fontend/vite.config.js
@@ -12,9 +12,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+  },
   server: {
-    proxy: {
-      '/api': 'http://localhost:5000', // Your backend
-    },
+    historyApiFallback: true,
   },
 })
