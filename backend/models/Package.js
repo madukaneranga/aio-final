879a206b8701b180fc5e7c02f0090d09b65da394
MadukaNernaga <141767341+MadukaNernaga@users.noreply.github.com>
2025-07-17 10:05:14 +0530

    Update Package.js

--- backend/models/Package.js
+++ backend/models/Package.js
@@ -3,13 +3,12 @@ import mongoose from "mongoose";
 const packageSchema = new mongoose.Schema({
   name: {
     type: String,
-    default:"basic"
+    default: "basic"
   },
   amount: {
     type: Number,
     default: 1500
-  },
+  }
 });
 
-
-export default mongoose.model("Package", subscriptionSchema);
+export default mongoose.model("Package", packageSchema);
