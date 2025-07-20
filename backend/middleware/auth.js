583424a97c6da258e8b8100c8a437ebce4749447
MadukaNernaga <141767341+MadukaNernaga@users.noreply.github.com>
2025-07-20 19:19:11 +0530

    Update auth.js

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -4,6 +4,7 @@ import User from '../models/User.js';
 export const authenticate = async (req, res, next) => {
   try {
     const token = req.header('Authorization')?.replace('Bearer ', '');
+    console.log(" JWT Token:", token);
     if (!token) {
       return res.status(401).json({ error: 'No token provided' });
     }
