426a1c7445363966a995c7f4f4ec5255a41b195a
MadukaNernaga <141767341+MadukaNernaga@users.noreply.github.com>
2025-07-22 18:30:19 +0530

    Fixed and final;ized

--- fontend/src/utils/notificationHelpers.js
+++ fontend/src/utils/notificationHelpers.js
@@ -1,17 +1,18 @@
+// utils/notificationHelpers.js
 import {
-  PackageCheck,    // For order updates
-  CalendarCheck,   // For booking updates
-  Star,            // For review updates
-  Gift,            // For promotions
-  ShieldAlert,     // For warnings
-  Megaphone,       // For announcements
+  PackageCheck,
+  CalendarCheck,
+  Star,
+  Gift,
+  ShieldAlert,
+  Megaphone,
 } from "lucide-react";
 
 export const typeIcons = {
-  order_update: <PackageCheck className="w-5 h-5" />,
-  booking_update: <CalendarCheck className="w-5 h-5" />,
-  review_update: <Star className="w-5 h-5" />,
-  promotion: <Gift className="w-5 h-5" />,
-  warning: <ShieldAlert className="w-5 h-5" />,
-  announcement: <Megaphone className="w-5 h-5" />,
+  order_update: PackageCheck,
+  booking_update: CalendarCheck,
+  review_update: Star,
+  promotion: Gift,
+  warning: ShieldAlert,
+  announcement: Megaphone,
 };
