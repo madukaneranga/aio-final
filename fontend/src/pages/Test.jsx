b52c64d67e93bbdb4fe35fd50a052eb094c48017
MadukaNernaga <141767341+MadukaNernaga@users.noreply.github.com>
2025-08-09 18:21:46 +0530

    commit

--- fontend/src/pages/Test.jsx
+++ fontend/src/pages/Test.jsx
@@ -861,23 +861,4 @@ const Test = () => {
 };
 
 export default Test;
-=======
-import React, { useRef } from 'react';
-
-function UncontrolledInput() {
-  const inputRef = useRef(null);
-
-  const handleClick = () => {
-    alert(`Input value is: ${inputRef.current.value}`);
-  };
-
-  return (
-    <>
-      <input type="text" ref={inputRef} placeholder="Uncontrolled input" />
-      <button onClick={handleClick}>Show Value</button>
-    </>
-  );
-}
-
-export default UncontrolledInput;
 
